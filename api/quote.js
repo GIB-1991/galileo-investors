36d4696136f0104154ff1d5171243a0a19a12f3f
GIB-1991 <gilbitan2000@gmail.com>
2026-03-29 08:48:34 +0300

    Force redeploy: 2026-03-29T05:48:33.523Z

--- api/quote.js
+++ api/quote.js
@@ -1,5 +1,6 @@
 // Fetch short interest from Yahoo Finance quoteSummary
 async function getYahooShort(symbol, ua) {
+  // Build: 2026-03-29T05:48:33.523Z
   try {
     const url = 'https://query2.finance.yahoo.com/v10/finance/quoteSummary/'+symbol+'?modules=defaultKeyStatistics';
     const r = await fetch(url, { headers: { 'User-Agent': ua, 'Accept': 'application/json', 'Accept-Language': 'en-US,en;q=0.9' } });
